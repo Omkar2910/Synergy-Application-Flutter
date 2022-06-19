05fd0ee781d35b66f26d3b6909acf8f7383b3334
Omkar <omkaringle29@gmail.com>
2022-06-19 09:17:04 +0530

    v17 - saving seller data to firestore and locally, (registration page seller app)

--- synergy_rider_app/android/app/src/main/kotlin/com/example/synergy_rider_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.synergy_rider_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- synergy_seller_app/lib/authentication/register.dart
+++ synergy_seller_app/lib/authentication/register.dart
@@ -1,8 +1,10 @@
-
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:seller_app/mainScreens/home_screen.dart';
 import 'package:seller_app/widgets/custom_text_field.dart';
 
 import 'package:geocoding/geocoding.dart';
@@ -10,6 +12,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:seller_app/widgets/error_dialog.dart';
 import 'package:seller_app/widgets/loading_dialog.dart';
 import 'package:firebase_storage/firebase_storage.dart' as fStorage;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -33,7 +36,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Position? position;
   List<Placemark>? placeMarks;
 
-  String? sellerImageUrl = "";
+  String sellerImageUrl = "";
+  String completeAddress = "";
 
   Future<void> _getimage() async {
     imageXFile = await _picker.pickImage(source: ImageSource.gallery);
@@ -59,79 +63,128 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
     Placemark pMark = placeMarks![0];
 
-    String completeAddress =
+    completeAddress =
         '${pMark.subThoroughfare} ${pMark.thoroughfare}, ${pMark.subLocality} ${pMark.locality}, ${pMark.subAdministrativeArea}, ${pMark.administrativeArea} ${pMark.postalCode}, ${pMark.country}';
 
     locationController.text = completeAddress;
   }
 
-  Future<void> formValidation() async
-  {
-      if(imageXFile==null)
-      {
-        showDialog(
-          context: context ,
-          builder: (c){
+  Future<void> formValidation() async {
+    if (imageXFile == null) {
+      showDialog(
+          context: context,
+          builder: (c) {
             return ErrorDialog(
               message: "Please select an image",
             );
-          }
-        );
-      }
-      else
-      {
-        if(passwordController.text == confirmPasswordController.text)
-        {
-          if(confirmPasswordController.text.isNotEmpty && emailController.text.isNotEmpty && phoneController.text.isNotEmpty && locationController.text.isNotEmpty)
-          {
-            // Start Uploading
-            showDialog(
-              context: context,
-              builder: (c)
-              {
-                return LoadingDialog(
-                  message: "Registering Account", // Custom Widget loading_dialog.dart
-                );
-              }
-
-            );
-
-            String fileName = DateTime.now().millisecondsSinceEpoch.toString();
-            fStorage.Reference reference = fStorage.FirebaseStorage.instance.ref().child("sellers").child(fileName);
-            fStorage.UploadTask uploadTask = reference.putFile(File(imageXFile!.path));
-            fStorage.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
-            await taskSnapshot.ref.getDownloadURL().then((url) {
-              sellerImageUrl = url;
-            });
-            
-          }
-          else
-          {
-            showDialog(
-          context: context ,
-          builder: (c){
-            return ErrorDialog(
-              message: "Please enter all the required details for Registration.",
-            );
-          }
-        );
-          }
-        }
-        else
-        {
+          });
+    } else {
+      if (passwordController.text == confirmPasswordController.text) {
+        if (confirmPasswordController.text.isNotEmpty &&
+            emailController.text.isNotEmpty &&
+            phoneController.text.isNotEmpty &&
+            locationController.text.isNotEmpty) {
+          // Start Uploading
           showDialog(
-          context: context ,
-          builder: (c){
-            return ErrorDialog(
-              message: "Password and ConfirmPassword does not match.",
-            );
-          }
-        );
+              context: context,
+              builder: (c) {
+                return LoadingDialog(
+                  message:
+                      "Registering Account", // Custom Widget loading_dialog.dart
+                );
+              });
+
+          String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+          fStorage.Reference reference = fStorage.FirebaseStorage.instance
+              .ref()
+              .child("sellers")
+              .child(fileName);
+          fStorage.UploadTask uploadTask =
+              reference.putFile(File(imageXFile!.path));
+          fStorage.TaskSnapshot taskSnapshot =
+              await uploadTask.whenComplete(() {});
+          await taskSnapshot.ref.getDownloadURL().then((url) {
+            sellerImageUrl = url;
+
+            //save info to firestore
+            authenticateSellerAndSignUp();
+          });
+        } else {
+          showDialog(
+              context: context,
+              builder: (c) {
+                return ErrorDialog(
+                  message:
+                      "Please enter all the required details for Registration.",
+                );
+              });
         }
+      } else {
+        showDialog(
+            context: context,
+            builder: (c) {
+              return ErrorDialog(
+                message: "Password and ConfirmPassword does not match.",
+              );
+            });
       }
+    }
+  }
 
+  void authenticateSellerAndSignUp() async {
+    User? currentUser;
+    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth
+        .createUserWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    )
+        .then((auth) {
+      currentUser = auth.user;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(
+              message: error.message.toString(),
+            );
+          });
+    });
 
-  } 
+    if (currentUser != null) {
+      saveDataToFirestore(currentUser!).then((value) {
+        Navigator.pop(context);
+        // send user to the home screen
+        Route newRoute = MaterialPageRoute(builder: (c) => HomeScreen());
+        Navigator.pushReplacement(context, newRoute);
+      });
+    }
+  }
+
+  Future saveDataToFirestore(User currentUser) async {
+    FirebaseFirestore.instance.collection("seller").doc(currentUser.uid).set({
+      "sellerUID": currentUser.uid,
+      "sellerEmail": currentUser.email,
+      "sellerName": nameController.text.trim(),
+      "sellerAvatarUrl": sellerImageUrl,
+      "phone": phoneController.text.trim(),
+      "address": completeAddress,
+      "status": "approved",
+      "earnings": 0.0,
+      "lat": position!.latitude,
+      "lng": position!.longitude,
+    });
+
+    //save data locally
+
+    SharedPreferences? sharedPreferences =
+        await SharedPreferences.getInstance();
+    await sharedPreferences.setString("uid", currentUser.uid);
+    await sharedPreferences.setString("email", currentUser.email.toString());
+    await sharedPreferences.setString("name", nameController.text.trim());
+    await sharedPreferences.setString("photoUrl", sellerImageUrl);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -241,8 +294,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
               ),
-              onPressed: ()
-              {
+              onPressed: () {
                 formValidation();
               },
               child: const Text("Sign Up",
